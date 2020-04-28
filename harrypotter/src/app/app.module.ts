00eb633974119cf074f06c16379d7706662f08e6
yonisGit <yoniel@post.bgu.ac.il>
2020-04-28 12:17:28 +0300

    minor changes

--- harrypotter/src/app/app.module.ts
+++ harrypotter/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HousesComponent } from './houses/houses.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HousesComponent
   ],
   imports: [
     BrowserModule,
---
+++ harrypotter/src/app/mock-houses.ts
@@ -0,0 +1,8 @@
+import { House } from './house';
+
+export const HOUSES: House[] = [
+  { id: 1, name: 'Gryffindor', image: 'src/assets/house_images/Gryffindor_House_Pride_Collections.svg'},
+  { id: 2, name: 'Hufflepuff', image: 'src/assets/house_images/Gryffindor_House_Pride_Collections.svg'},
+  { id: 3, name: 'Ravenclaw', image: 'src/assets/house_images/Gryffindor_House_Pride_Collections.svg'},
+  { id: 4, name: 'Slytherin', image: 'src/assets/house_images/Gryffindor_House_Pride_Collections.svg'}
+];
