cdf79a227dfce8cfdeb5ec1754468ab23157ef37
yonisGit <yoniel@post.bgu.ac.il>
2020-05-24 11:56:31 +0300

    fixed pipe problems

--- harrypotter/src/app/sort-by-name.pipe.ts
+++ harrypotter/src/app/sort-by-name.pipe.ts
@@ -7,24 +7,21 @@ import {PipeType} from './pipe-type';
 })
 export class SortOrFilterPipe implements PipeTransform {
 
-  transform1(wizards: Wizard[], sortType: PipeType): any {
-
-  }
-
-  transform(wizardsUnsorted: Wizard[], whichSort: number): Wizard[] {
-    // todo: the whichSort field will be a string indicating the property name (enum) .
-    let wizardsSorted: Wizard[]; // todo: dont need this
-    // todo: rename wizardsUnsorted to unsortedWizards
-    if (whichSort === 1) {
-      wizardsSorted = wizardsUnsorted.sort((a, b) =>
-        a.name > b.name ? 1 : -1); // todo: rename a&b
-      return wizardsSorted;
-    } else if (whichSort === 2) {
-      wizardsSorted = wizardsUnsorted.sort((a, b) =>
-        a.age > b.age ? 1 : -1);
-      return wizardsSorted;
+  transform(unsortedWizards: Wizard[], sortType: PipeType): any {
+    if (sortType === PipeType.Sort_Name) {
+      return unsortedWizards.sort((wizard1, wizard2) =>
+        wizard1.name > wizard2.name ? 1 : -1);
+    } else if (sortType === PipeType.Sort_Name_Dec) {
+      return unsortedWizards.sort((wizard1, wizard2) =>
+        wizard1.name > wizard2.name ? -1 : 1);
+    } else if (sortType === PipeType.Sort_Age) {
+      return unsortedWizards.sort((wizard1, wizard2) =>
+        wizard1.age > wizard2.age ? 1 : -1);
+    } else if (sortType === PipeType.Sort_Age_Dec) {
+      return unsortedWizards.sort((wizard1, wizard2) =>
+        wizard1.age > wizard2.age ? -1 : 1);
     } else {
-      return wizardsUnsorted;
+      return unsortedWizards;
     }
   }
 
