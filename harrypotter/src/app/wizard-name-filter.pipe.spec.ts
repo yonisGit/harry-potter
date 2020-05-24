1b7a67098f25adecf29ed4cbf455965254914644
yonisGit <yoniel@post.bgu.ac.il>
2020-05-24 12:08:08 +0300

    handled all filters

---
+++ harrypotter/src/app/wizard-name-filter.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { WizardNameFilterPipe } from './wizard-name-filter.pipe';
+
+describe('WizardNameFilterPipe', () => {
+  it('create an instance', () => {
+    const pipe = new WizardNameFilterPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
