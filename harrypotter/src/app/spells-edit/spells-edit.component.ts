c276c4fbb811f0f9a4a6b8cbcfb9606dfee078d3
yonisGit <yoniel@post.bgu.ac.il>
2020-05-25 11:13:01 +0300

    refactored add func

--- harrypotter/src/app/spells-edit/spells-edit.component.ts
+++ harrypotter/src/app/spells-edit/spells-edit.component.ts
@@ -24,7 +24,7 @@ export class SpellsEditComponent implements OnInit {
   @ViewChild('spellInput') spellInput: ElementRef<HTMLInputElement>;
   @ViewChild('auto') matAutocomplete: MatAutocomplete;
 
-  // todo: constructor always first and after it the ngOnInit.
+  // todo: constructor always first and after it the ngOnInit. ---> DONE
   constructor(private spellsService: SpellsService) {
   }
 
@@ -33,21 +33,21 @@ export class SpellsEditComponent implements OnInit {
     this.getSpells();
   }
 
-  // todo: refactor function
+  // todo: refactor function ---> kind of DONE
   add(event: MatChipInputEvent): void {
     const input = event.input;
     const value = event.value;
 
-    // Add our spell
     if (value.trim()) { // todo: remove trim and '' everywhere. ---> DONE
-      const newSpell = {id: this.allSpells.findIndex(s => s.name === value), name: value.trim()};
-      if (this.allSpells.map(s => s.name).includes(newSpell.name)) {
-        if (!this.wizardSpells.includes(newSpell.name)) {
-          this.wizardSpells.push(newSpell.name);
-          this.wizard.spells = this.wizardSpells;
-        } else {
-          alert('The ' + newSpell.name + ' spell is already exists for this wizard! Sorry...');
-        }
+      const newSpell =
+        {
+          id: this.allSpells.findIndex(spell => spell.name === value),
+          name: value.trim()
+        };
+
+      const isRealSpell = this.allSpells.map(spell => spell.name).includes(newSpell.name);
+      if (isRealSpell) {
+        this.addSpellToWizard(newSpell);
       } else {
         alert('The ' + newSpell.name + ' spell doesn\'t exist in the allowed spell list! Sorry...');
       }
@@ -58,8 +58,17 @@ export class SpellsEditComponent implements OnInit {
     }
   }
 
+  private addSpellToWizard(newSpell: { name: string; id: number }) {
+    if (!this.wizardSpells.includes(newSpell.name)) {
+      this.wizardSpells.push(newSpell.name);
+      this.wizard.spells = this.wizardSpells;
+    } else {
+      alert('The ' + newSpell.name + ' spell is already exists for this wizard! Sorry...');
+    }
+  }
+
   remove(spell: string): void {
-    const index = this.wizardSpells.indexOf(spell); // todo: use includes
+    const index = this.wizardSpells.indexOf(spell); // todo: use includes ---> I need the index here so its indexOf
 
     if (index >= 0) {
       this.wizardSpells.splice(index, 1);
